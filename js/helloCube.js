e40906d8a1eeeaff37ed975704426a932ea23768
CapeGenius <90223034+CapeGenius@users.noreply.github.com>
2022-07-26 09:43:08 -0500

    Add files via upload

---
+++ js/helloCube.js
@@ -0,0 +1,25 @@
+<script type="module">
+    import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js';
+    
+    function main() {
+        const canvas = document.querySelector('#c');
+        const renderer = new THREE.WebGLRenderer({canvas});
+    }
+
+</script>
+
+
+
+class helloCube {
+
+<body>
+  < canvas id="c">
+    const fov = 75;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  </canvas>
+</body>
+
+}
